88340a52886f87e4d0339d27493458632b19c0a3
Xavi Aznar <xavi.aznar@outlook.com>
2022-08-28 09:39:06 +0000

    Reorganize funcitons in files (for reusability)

--- cmd/processor/main.go
+++ cmd/processor/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"io/fs"
 	"log"
 	"os"
 	"os/signal"
-	"path/filepath"
 	"queue/models"
 	"syscall"
 	"time"
@@ -22,73 +19,9 @@ func main() {
 		case <-c:
 			log.Println("Exiting the app...")
 			return
-		case <-time.After(1 * time.Second):
+		case <-time.After(5 * time.Second):
 			log.Println("processing pending jobs...")
-			pendingJobs()
+			models.PendingJobs()
 		}
 	}
-}
-
-func pendingJobs() {
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Fatalf("[error] Failed to get current working directory '%s", err.Error())
-	}
-	var pendingJobs []string
-	// filepath.WalkDir (go1.16) is more efficient thant filepath.Walk
-	err = filepath.WalkDir(cwd, func(path string, d fs.DirEntry, err error) error {
-		if filepath.Ext(path) == ".pending" {
-			pendingJobs = append(pendingJobs, path)
-		}
-		return nil
-	})
-	if err != nil {
-		log.Fatalf("[error] Failed walk dir %s", err.Error())
-	}
-	for _, j := range pendingJobs {
-		var job models.Job
-		job, err = readJob(j)
-		if err != nil {
-			log.Printf("[error] failed to read job from file: %s", err.Error())
-			return
-		}
-		if err = updateJob(job); err != nil {
-			log.Printf("[error] failed to update job %s: %s", job.Id, err.Error())
-		}
-	}
-}
-
-func readJob(jobFile string) (models.Job, error) {
-	var job = models.Job{}
-
-	jobBytes, err := os.ReadFile(jobFile)
-	if err != nil {
-		log.Printf("[error] failed to read file %s: %s", jobFile, err.Error())
-		return job, err
-	}
-	if err := json.Unmarshal(jobBytes, &job); err != nil {
-		log.Printf("[error] failed to parse file '%s': '%s'", jobFile, err.Error())
-		return job, err
-	}
-	return job, nil
-}
-
-func updateJob(job models.Job) error {
-	job.LastUpdated = time.Now()
-	job.Result = job.Num1 + job.Num2
-	j, err := json.Marshal(job)
-	if err != nil {
-		log.Fatalf("[ ERROR ] Failed to convert struct to JSON '%s'\n", err.Error())
-		return err
-	}
-	if err := os.WriteFile(job.Id+".json", []byte(j), 0664); err != nil {
-		log.Fatalf("[ERROR] Failed to update job '%s', '%s'", job.Id, err.Error())
-		return err
-	}
-	if err := os.Remove(job.Id + ".pending"); err != nil {
-		log.Printf("[ERROR] Failed to delete file '%s', '%s'", job.Id+".pending", err.Error())
-		return err
-	}
-	log.Printf("[ ok ] processed jobId '%s' successfully\n", job.Id)
-	return nil
 }
--- models/models.go
+++ models/models.go
@@ -2,8 +2,10 @@ package models
 
 import (
 	"encoding/json"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/google/uuid"
@@ -35,4 +37,68 @@ func CreateJob(n1, n2 int) (uuid.UUID, error) {
 	os.WriteFile(jobid.String()+".pending", []byte(j), 0664)
 
 	return jobid, nil
+}
+
+func PendingJobs() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("[error] Failed to get current working directory '%s", err.Error())
+	}
+	var pendingJobs []string
+	// filepath.WalkDir (go1.16) is more efficient thant filepath.Walk
+	err = filepath.WalkDir(cwd, func(path string, d fs.DirEntry, err error) error {
+		if filepath.Ext(path) == ".pending" {
+			pendingJobs = append(pendingJobs, path)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("[error] Failed walk dir %s", err.Error())
+	}
+	for _, j := range pendingJobs {
+		var job Job
+		job, err = readJob(j)
+		if err != nil {
+			log.Printf("[error] failed to read job from file: %s", err.Error())
+			return
+		}
+		if err = updateJob(job); err != nil {
+			log.Printf("[error] failed to update job %s: %s", job.Id, err.Error())
+		}
+	}
+}
+
+func readJob(jobFile string) (Job, error) {
+	var job = Job{}
+
+	jobBytes, err := os.ReadFile(jobFile)
+	if err != nil {
+		log.Printf("[error] failed to read file %s: %s", jobFile, err.Error())
+		return job, err
+	}
+	if err := json.Unmarshal(jobBytes, &job); err != nil {
+		log.Printf("[error] failed to parse file '%s': '%s'", jobFile, err.Error())
+		return job, err
+	}
+	return job, nil
+}
+
+func updateJob(job Job) error {
+	job.LastUpdated = time.Now()
+	job.Result = job.Num1 + job.Num2
+	j, err := json.Marshal(job)
+	if err != nil {
+		log.Fatalf("[ ERROR ] Failed to convert struct to JSON '%s'\n", err.Error())
+		return err
+	}
+	if err := os.WriteFile(job.Id+".json", []byte(j), 0664); err != nil {
+		log.Fatalf("[ERROR] Failed to update job '%s', '%s'", job.Id, err.Error())
+		return err
+	}
+	if err := os.Remove(job.Id + ".pending"); err != nil {
+		log.Printf("[ERROR] Failed to delete file '%s', '%s'", job.Id+".pending", err.Error())
+		return err
+	}
+	log.Printf("[ ok ] processed jobId '%s' successfully\n", job.Id)
+	return nil
 }
