87bddb4b1f4eff84a7f7531cd0087a7385a3f019
Xavi Aznar <xavi.aznar@outlook.com>
2022-09-16 18:07:48 +0200

    Remove -interval (merged into -loop)
    If -interval is != 0, "cleaner" runs in a loop.
    The value provided indicates the duration of each loop.
    
    If no value is provided, the default value is 0, so cleaner just runs once.

--- cmd/cleaner/main.go
+++ cmd/cleaner/main.go
@@ -11,27 +11,25 @@ import (
 )
 
 func main() {
-	var interval time.Duration
+	var loop time.Duration
 	var max_age time.Duration
-	var loop bool
-	flag.DurationVar(&interval, "interval", 3*time.Second, "When running with -loop, interval between every check for outdated jobs")
+	flag.DurationVar(&loop, "interval", 0, "If a value is provided, run every (value); if the value is 0, run just once")
 	flag.DurationVar(&max_age, "max_age", 1*time.Hour, "Remove jobs older than max_age")
-	flag.BoolVar(&loop, "loop", false, "Run the cleaner app in a loop")
 	flag.Parse()
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	if loop {
+	if loop != 0 {
 		// Main loop
-		log.Printf("cleaning jobs older than '%s' every '%s' ...\n", max_age.String(), interval.String())
+		log.Printf("cleaning jobs older than '%s' every '%s' ...\n", max_age.String(), loop.String())
 		for {
 			select {
 			case <-c:
 				log.Println("Exiting the app...")
 				return
-			case <-time.After(interval):
-				log.Printf("cleaning jobs older than '%s' every '%s' ...\n", max_age.String(), interval.String())
+			case <-time.After(loop):
+				log.Printf("cleaning jobs older than '%s' every '%s' ...\n", max_age.String(), loop.String())
 				models.CleanJobs(max_age)
 			}
 		}
